bd58c6cf208ef43bc8d1117cd3b3caa311771373
Ben Rosser <rosser.bjr@gmail.com>
2016-01-04 17:00:22 -0500

    Fix a couple of whitespace errors in core.{cpp,h}

--- src/core/core.cpp
+++ src/core/core.cpp
@@ -917,7 +917,7 @@ bool Core::changeUserPass(const QString &username)
         out << "User " << username << " is configured through an auth provider that has forbidden manual password changing." << endl;
         return false;
     }
-    
+
     out << "Change password for user: " << username << endl;
 
     disableStdInEcho();
